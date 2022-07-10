1ac9739715325acdbc5c6f4c396d55855fc6e316
akshaykanherkar7 <akshaykanherkar7@gmail.com>
2022-07-10 12:35:12 +0530

    Added Code

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,45 +1,45 @@
 /// <reference types="Cypress" />
 
-// describe("Todo Cypress Testing 1", () => {
-//   beforeEach(() => {
-//     cy.visit("http://localhost:3000");
-//   });
+describe("Todo Cypress Testing 1", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
 
-//   it("input form should be auto-focused", () => {
-//     cy.get(".input_todo").focus();
-//   });
+  it("input form should be auto-focused", () => {
+    cy.get(".input_todo").focus();
+  });
 
-//   it("select the input element, using a class", () => {
-//     cy.get(".input_todo").type("Mahesh").should("have.value", "Mahesh");
-//   });
-// });
+  it("select the input element, using a class", () => {
+    cy.get(".input_todo").type("Mahesh").should("have.value", "Mahesh");
+  });
+});
 
-// describe("Todo Cypress Testing 2", () => {
-//   beforeEach(() => {
-//     cy.visit("http://example.cypress.io/todo");
-//   });
+describe("Todo Cypress Testing 2", () => {
+  beforeEach(() => {
+    cy.visit("http://example.cypress.io/todo");
+  });
 
-//   it("should be able to add a task", () => {
-//     cy.get(".todo-list").children().should("have.length", 2);
-//     cy.get(".new-todo").type("Learn React{enter}");
-//     cy.get(".new-todo").type("Learn Redux{enter}");
-//     cy.get(".new-todo").type("Learn Cypress{enter}");
-//     cy.get(".todo-list").children().should("have.length", 5);
-//   });
-// });
+  it("should be able to add a task", () => {
+    cy.get(".todo-list").children().should("have.length", 2);
+    cy.get(".new-todo").type("Learn React{enter}");
+    cy.get(".new-todo").type("Learn Redux{enter}");
+    cy.get(".new-todo").type("Learn Cypress{enter}");
+    cy.get(".todo-list").children().should("have.length", 5);
+  });
+});
 
-// describe("Todo Cypress Testing 3", () => {
-//   beforeEach(() => {
-//     cy.visit("http://example.cypress.io/todo");
-//   });
+describe("Todo Cypress Testing 3", () => {
+  beforeEach(() => {
+    cy.visit("http://example.cypress.io/todo");
+  });
 
-//   it("should be able to toggle a task", () => {
-//     cy.get(".new-todo").type("Learn React{enter}");
-//     cy.get(".new-todo").type("Learn Redux{enter}");
-//     cy.get(".new-todo").type("Learn Cypress{enter}");
-//     cy.get(".toggle").first().click().get("li").should("have.class", "completed");
-//   });
-// });
+  it("should be able to toggle a task", () => {
+    cy.get(".new-todo").type("Learn React{enter}");
+    cy.get(".new-todo").type("Learn Redux{enter}");
+    cy.get(".new-todo").type("Learn Cypress{enter}");
+    cy.get(".toggle").first().click().get("li").should("have.class", "completed");
+  });
+});
 
 describe("Todo Cypress Testing 4", () => {
   beforeEach(() => {
